78b49092a9cb338d31cd5b77c0e8bf84b7d7a16d
Daniel Valenzuela <danielamigos@gmail.com>
2015-07-29 21:05:54 -0500

    initial sprint

--- wp-content/themes/bluesunmarketing/template-scrollingpage.php
+++ wp-content/themes/bluesunmarketing/template-scrollingpage.php
@@ -14,71 +14,305 @@
                         <div class="full-width-image-text">
                             <h2>Retail Product Marketing <br/>at its best.</h2>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/Front-Page-Image-1.png" alt="" />                        
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/Front-Page-Image-1.png" alt="" />      
                     </div>
-                    <div class="section-heading">
-                        <h1 class="heading-1">A Better Broker For</h1>
-                        <h1 class="heading-2">Your Brand.</h1>
+                    <div class="section-heading white-text">
+                        <h1 class="top-heading light-heading">A Better Broker For</h1>
+                        <h1 class="bottom-heading bold-heading">Your Brand.</h1>
                     </div>
-                    <div class="section-content">
-                        
-                    </div>
-                    
+                    <div class="section-content white-text">
+                        <p><img src="<?php echo get_template_directory_uri(); ?>/img/chart.jpg" alt="" /></p>
+                        <p class="p1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
+                    </div>                    
                 </div>
             </div>
         </div>
     </section>
 
     <!-- About Section -->
-    <section id="scrolling-section-2" class="scrolling-section">
+    <section id="scrolling-section-2" class="scrolling-section" style="background-color:#128e9b">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>About Section</h1>
+                    <div class="section-heading white-text">
+                        <h1 class="top-heading bold-heading">Your grow</h1>
+                        <h1 class="bottom-heading light-heading">We grow.</h1>
+                    </div>                    
+                    <div class="section-content white-text">
+                        <br />
+                        <br />
+                        <p class="p1">[Illustration that symbolizes the relationship between the broker and company]</p>
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <p class="p1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
+                        <br />
+                        <p><img class="plus-sign" data-expand-class="explandable-content" src="<?php echo get_template_directory_uri(); ?>/img/icon-plus.png" alt="" /></p>
+                        <p class="p1 explandable-content" style="display:none;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
+                    </div>  
                 </div>
             </div>
         </div>
     </section>
 
     <!-- Services Section -->
-    <section id="scrolling-section-3" class="scrolling-section">
+    <section id="scrolling-section-3" class="scrolling-section" style="background-color:#f1f1f2">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12">
-                    <h1>Services Section</h1>
+                <div class="col-lg-12"> 
+                    <div class="section-heading blue-text">
+                        <h1 class="top-heading light-heading">Our Expertise</h1>
+                        <h1 class="bottom-heading bold-heading">Speaks for Itself.</h1>
+                    </div>                    
+                    <div class="section-content blue-text">
+                        <br />
+                        <br />
+                        <p class="p1">[Illustration that symbolizes ideas/big picture thinking offered in the services.]</p>
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <div class="row p3" style="color:#1aa3dd;font-weight:700;">
+                            <div class="col-md-3 ">
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    Account Specific Marketing
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    Logistics
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    Program Negotiations and Service Issue Resolutions
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    Outsourced Sales and Placement of Suppliers Products subject to any terms, conditions, and Prices of the supplier
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    Distribution
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    Guidance on Consumer Marketing
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    Merchandising Advice
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    Product Development Consulting
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    Customer Interface
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    Operations Support
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    In-Store/Club Marketing Management As Required
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                    Administrative Support Function (order processing support)
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <br />
+                    </div>  
                 </div>
             </div>
         </div>
     </section>
 
     <!-- Contact Section -->
-    <section id="scrolling-section-4" class="scrolling-section">
+    <section id="scrolling-section-4" class="scrolling-section" style="background-color:#0c5485">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Contact Section</h1>
+                    <div class="section-heading white-text">
+                        <h1 class="top-heading light-heading">Look at Our</h1>
+                        <h1 class="bottom-heading bold-heading">Client Successes.</h1>
+                    </div>  
+                                 
+                                 
+             <div class="catapult-slideshow">
+				<div class="catapult-slideshow-wrapper" style="">
+					<div class="slideshow-left-arrow">
+						<a class="previous-slide" href='#'><img src='<?php echo get_template_directory_uri(); ?>/img/slider-arrow-left.png' alt='Previous' ></a>
+					</div>
+					<div class="slideshow-right-arrow">
+						<a class="next-slide" href='#'><img src='<?php echo get_template_directory_uri(); ?>/img/slider-arrow-right.png' alt='Next' ></a>
+					</div>				
+					<div class="catapult-slide">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/products/bsm-nuttzo-1.png" alt="" />
+					</div>			
+					<div class="catapult-slide">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/products/bsm-piratesbooty-1.png" alt="" />
+					</div>			
+					<div class="catapult-slide">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/products/bsm-goldenisland-1.png" alt="" />
+					</div>						
+				</div>
+			</div>
+                                 
+                                 
+                                 
                 </div>
             </div>
         </div>
     </section>
     <!-- Contact Section -->
-    <section id="scrolling-section-5" class="scrolling-section">
+    <section id="scrolling-section-5" class="scrolling-section" style="background-color:#103548">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Contact Section</h1>
+                    <div class="section-heading white-text">
+                        <h1 class="top-heading light-heading">Meet Our</h1>
+                        <h1 class="bottom-heading bold-heading">Team.</h1>
+                    </div>  
+                    <div class="section-content white-text">
+                        <br />
+                        <br />
+                        <p class="p1">[Illustration that symbolizes the team effort of the company.]</p>
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <p class="p1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
+                        <br />
+                    </div>
+                    
+                    <div style=" background-color:#ffffff;" class="row">
+                        <div class="col-md-6"> 
+        					<div class="slideshow-left-arrow">
+        						<a class="previous-slide" href='#'><img src='<?php echo get_template_directory_uri(); ?>/img/slider-arrow-left.png' alt='Previous' ></a>
+        					</div>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <div class="section-heading blue-text">
+                                <h1 class="top-heading bold-heading">Perry</h1>
+                                <h1 class="bottom-heading light-heading">Abider.</h1>
+                            </div>  
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                        </div>
+                        <div class="col-md-6">
+        					<div class="slideshow-right-arrow">
+        						<a class="next-slide" href='#'><img src='<?php echo get_template_directory_uri(); ?>/img/slider-arrow-right.png' alt='Next' ></a>
+        					</div>		
+                            <br/>
+                            <br/>
+                            <br/>	
+                            <p class="p1" style="text-align:left;font-family: 'Open Sans Condensed', sans-serif;color:#1aa3dd">President/CEO</p>
+                            <p class="p3" style="padding-right:70px; text-align:left;font-family: 'Open Sans Condensed', sans-serif;color:#1aa3dd">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                        </div>                        
+                    </div>
                 </div>
             </div>
         </div>
     </section>
     <!-- Contact Section -->
-    <section id="scrolling-section-6" class="scrolling-section">
+    <section id="scrolling-section-6" class="scrolling-section" style="background-color:#128e9b">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Scrolling Nav</h1>
-                    <p><strong>Usage Instructions:</strong> Make sure to include the <code>scrolling-nav.js</code>, <code>jquery.easing.min.js</code>, and <code>scrolling-nav.css</code> files. To make a link smooth scroll to another section on the page, give the link the <code>.page-scroll</code> class and set the link target to a corresponding ID on the page.</p>
-                    <a class="btn btn-default page-scroll" href="#about">Click Me to Scroll Down!</a>
+                    <div class="section-heading white-text">
+                        <h1 class="top-heading bold-heading">Take a look</h1>
+                        <h1 class="bottom-heading light-heading">at Our Locations.</h1>
+                    </div> 
+                            <br/>	
+                            <br/>
+                            <br/>
+                            <br/>	
+                            <br/>
+                            <br/>		
+                    <div style=" background-color:#ffffff;" class="row">
+                        <div class="col-md-6"> 
+        					<div class="slideshow-left-arrow">
+        						<a class="previous-slide" href='#'><img src='<?php echo get_template_directory_uri(); ?>/img/slider-arrow-left.png' alt='Previous' ></a>
+        					</div>
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/location.jpg" alt="Location"/>
+                        </div>
+                        <div class="col-md-6">
+        					<div class="slideshow-right-arrow">
+        						<a class="next-slide" href='#'><img src='<?php echo get_template_directory_uri(); ?>/img/slider-arrow-right.png' alt='Next' ></a>
+        					</div>		
+                            <br/>
+                            <br/>
+                            <br/>	
+                            <br/>
+                            <br/>	
+                            <p class="p1" style="text-align:left;font-family: 'Open Sans Condensed', sans-serif;color:#1aa3dd">345 Hartz Ave, Danville Ca 94526</p>
+                            <p class="p3" style="padding-right:70px; text-align:left;font-family: 'Open Sans Condensed', sans-serif;color:#1aa3dd">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
+                            <br/>
+                            <br/>
+                            <br/>
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> 
+    
+    <section id="scrolling-section-7" class="scrolling-section" style="background-color:#0c5485">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-heading white-text">
+                        <h1 class="top-heading light-heading">We're</h1>
+                        <h1 class="bottom-heading bold-heading">Ready.</h1>
+                    </div>  
+                                 
+                     <div class="section-content white-text">
+                        <br />
+                        <br />
+                        <p class="p1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
+                        
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <p><button class="btn btn-warning btn-lg" style="font-size:2rem;padding:20px 100px;">BEGIN HERE</button></p>
+                    </div>  
+            
+                                 
+                                 
+                                 
                 </div>
             </div>
         </div>
